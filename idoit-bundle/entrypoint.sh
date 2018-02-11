cb9edfb4bca55d77c8a092e7e8402e220e037d84
Benno <benno.weiss@gmail.com>
2018-02-11 14:40:21 +0100

    added i-doit bundle image

---
+++ idoit-bundle/entrypoint.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "Starting mariadb..."
+/bin/sh -c 'mysqld --user mysql &'
+sleep 2
+
+# THis block runs only on first start
+if [ ! -f /initial ]; then
+  echo "First start..."
+  echo "Running i-doit setup. This can take a while..."
+
+  # Set mariadb superuser password
+  mysql -uroot -ppassword -e "USE mysql; FLUSH PRIVILEGES; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MARIADB_SUPERUSER_PASSWORD}' WITH GRANT OPTION;"
+
+  # Run i-doit setup
+  cd $INSTALL_DIR/setup
+  ./install.sh -n ${IDOIT_TENANT} -s "idoit_system" -m "idoit_data" -h localhost -p $MARIADB_SUPERUSER_PASSWORD -a $IDOIT_ADMINCENTER_PASSWORD -q
+
+  # Create database user for i-doit
+  echo "Preparing database..."
+  mysql -uroot -p$MARIADB_SUPERUSER_PASSWORD -e "GRANT ALL PRIVILEGES ON idoit_data.* TO ${MARIADB_IDOIT_USERNAME}@'%' IDENTIFIED BY '${MARIADB_IDOIT_PASSWORD}';"
+  mysql -uroot -p$MARIADB_SUPERUSER_PASSWORD -e "GRANT ALL PRIVILEGES ON idoit_system.* TO ${MARIADB_IDOIT_USERNAME}@'%' IDENTIFIED BY '${MARIADB_IDOIT_PASSWORD}';"
+  mysql -uroot -pMARIADB_SUPERUSER_PASSWORD -e "UPDATE idoit_system.isys_mandator SET isys_mandator__db_user = '${MARIADB_IDOIT_USERNAME}', isys_mandator__db_pass = '${MARIADB_IDOIT_PASSWORD}';"
+
+  # Change config to use new mariadb user
+  echo "Setting up i-doit config..."
+  sed -i "s/\"user\" => \"root\"/\"user\" => \"${MARIADB_IDOIT_USERNAME}\"/g" ${INSTALL_DIR}/src/config.inc.php
+  sed -i "s/\"pass\" => \"${MARIADB_SUPERUSER_PASSWORD}\"/\"pass\" => \"${MARIADB_IDOIT_PASSWORD}\"/g" ${INSTALL_DIR}/src/config.inc.php
+  chown "$APACHE_USER":"$APACHE_GROUP" ${INSTALL_DIR}/src/config.inc.php
+
+  # Make sure bootstrapping happens only once
+  touch /initial
+fi
+
+echo "Starting webserver..."
+apache2ctl start
+
+
+echo "i-doit up and running..."
+dockerize -stderr /var/log/mysql/error.log -stderr /var/log/apache2/error.log -stderr /var/www/html/log/system -stderr /var/www/html/log/exception.log 
+
